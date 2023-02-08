2ce6ab1f85e0f894880edd6dfdcec7396a35d102
Doman-sp-qs <122662138+Doman-sp-qs@users.noreply.github.com>
2023-02-08 06:30:33 +0000

    [Add] クラスメソッドの書き方

---
+++ 2-9/new.rb
@@ -0,0 +1,23 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+  
+  def turn(direction)
+    puts "#{direction}に曲がります"
+  end
+end
+
+car = Car.new
+car.run(5)
+
+car = Car.new
+car.turn("右")
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
